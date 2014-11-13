3d8e8f3a50f16661c097a8f2134c5133bad11f01
JiangXD <jwcfx086@fudan.edu.cn>
2014-11-13 11:57:59 +0800

    Update blog.R

--- xyh/blog.R
+++ xyh/blog.R
@@ -4,4 +4,5 @@ mydat = data.frame(mydate=as.Date(scan(pipe("ls cn/_posts/*.md | sed 's,^[^0-9]*
 
 png("blog.png")
 ggplot(mydat, aes(x=mydate, y=mysize)) + geom_point(alpha=0.3, color="darkgreen") + geom_smooth() + labs(x="\nDate", y="Blog Size (Bytes)\n")
+
 dev.off()
